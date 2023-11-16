e320a1798f063d351e9ca63d3ca020243ec0d875
IlyaEliseev <84148446+IlyaEliseev@users.noreply.github.com>
2023-11-16 22:05:18 +0300

    update sql file

--- SelectProduct.sql
+++ SelectProduct.sql
@@ -1,3 +1,4 @@
-SELECT p.Name as ProductName, c.Name as CategoryName FROM Products as p
+SELECT p.Name as ProductName, c.Name as CategoryName 
+FROM Products as p
 LEFT JOIN ProductsCategories as pc on p.Id = pc.ProductId
 LEFT JOIN Categories as c on c.Id = pc.CategoryId
